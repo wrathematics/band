21d382e30d8134c8469c1b81ce920a204b673595
Drew Schmidt <wrathematics@gmail.com>
2016-05-19 13:11:47 -0400

    basic band package help skeleton

---
+++ R/band-package.r
@@ -0,0 +1,14 @@
+#' Banded Matrices
+#' 
+#' @description
+#' TODO
+#' 
+#' @import methods
+#' 
+#' @useDynLib band
+#' 
+#' @docType package
+#' @name band-package
+#' @author Drew Schmidt
+#' @keywords package
+NULL
