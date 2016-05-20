b1681edf1a4fa0ca2c891d0d6119cc831ef619dc
Drew Schmidt <wrathematics@gmail.com>
2016-05-20 13:24:46 -0400

    better testing

--- tests/banded.r
+++ tests/banded.r
@@ -1,27 +1,34 @@
 library(band)
 
 ### Diagonal band
+
+diagtest <- function(x)
+{
+  truth <- diag(x)
+  band <- as.banded(x, 0, 0)
+  test <- band@Data
+  stopifnot(all.equal(truth, test))
+
+  truth <- diag(diag(x))
+  # R makes literally the worst decisions about type, it's simply astounding
+  if (is.logical(x)) 
+    storage.mode(truth) <- "logical"
+  test <- as.matrix(band)
+  stopifnot(all.equal(truth, test))
+}
+
+# int
 x <- matrix(1:100, 10, 10)
-
-truth <- diag(x)
-band <- as.banded(x, 0, 0)
-test <- band@Data
-stopifnot(all.equal(truth, test))
-
-truth <- diag(diag(x))
-test <- as.matrix(band)
-stopifnot(all.equal(truth, test))
+diagtest(x)
 
 # double
 storage.mode(x) <- "double"
+diagtest(x)
 
-truth <- diag(x)
-band <- as.banded(x, 0, 0)
-test <- band@Data
-stopifnot(all.equal(truth, test))
+# logical
+storage.mode(x) <- "logical"
 
-truth <- diag(diag(x))
-test <- as.matrix(band)
-stopifnot(all.equal(truth, test))
 
-### 
+
+### General band
+# TODO
