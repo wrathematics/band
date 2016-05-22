84a931ae79098e9746db9a00de8d660a98c2efd9
Drew Schmidt <wrathematics@gmail.com>
2016-05-22 13:29:48 -0400

    add double test to isSymmetric()

--- tests/isSymmetric.R
+++ tests/isSymmetric.R
@@ -1,8 +1,17 @@
 library(band)
 
-x <- matrix(1:100, 10)
-test <- as.banded(as.matrix(as.symmetric(x)), 2)
-stopifnot(isSymmetric(test))
+symtests <- function(x)
+{
+  test <- as.banded(as.matrix(as.symmetric(x)), 2)
+  stopifnot(isSymmetric(test))
 
-test <- as.banded(x, 2, 2)
-stopifnot(!isSymmetric(test))
+  test <- as.banded(x, 2, 2)
+  stopifnot(!isSymmetric(test))
+}
+
+
+x <- matrix(1:100, 10)
+symtests(x)
+
+storage.mode(x) <- "double"
+symtests(x)
