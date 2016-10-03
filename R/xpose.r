9d16f5cebafddfdd68204e70330dde42f9164e84
wrathematics <wrathematics@gmail.com>
2016-10-03 17:41:02 -0400

    use xpose instead of t()

---
+++ R/xpose.r
@@ -0,0 +1,54 @@
+#' xpose
+#' 
+#' Transpose a matrix-like thing.
+#' 
+#' @param x
+#' A banded matrix.
+#' 
+#' @return
+#' A matrix-like object of the same type as the input.
+#' 
+#' @keywords LinAlg
+#' @export
+xpose <- function(x) UseMethod("xpose", x)
+
+xpose_err <- function(x)
+{
+  if (class(x) == "ZeroMat")
+    nm <- "zero"
+  else if (class(x) == "DiagMat")
+    nm <- "diagonal"
+  else if (class(x) == "SymMat")
+    nm <- "symmetric"
+  
+  stop(paste("a", nm, "matrix is its own transpose"))
+}
+
+#' @export
+xpose.BandMat <- xpose_err
+
+#' @rdname xpose
+xpose.SymMat <- xpose_err
+
+#' @export
+xpose.GenBandMat <- function(x)
+{
+  dim <- getdim(x)
+  k <- kdim(x)
+  
+  out <- .Call(R_xpose_band, getData(x), dim[1L], dim[2L], k[1L], k[2L])
+  genbandmat(out, rev(dim), k[2L], k[1L])
+}
+
+#' @export
+xpose.matrix <- function(x)
+{
+  dims <- dim(x)
+  if (any(dims == 1L))
+  {
+    dim(x) <- rev(dims)
+    return(x)
+  }
+  
+  .Call(R_xpose_full, x)
+}
--- tests/xpose.R
+++ tests/xpose.R
@@ -3,26 +3,26 @@ library(band)
 x <- matrix(1:100, 10)
 
 ### Degenerate cases
-test <- tryCatch(t(as.banded(x, 0, 0)), error=identity)
+test <- tryCatch(xpose(as.banded(x, 0, 0)), error=identity)
 stopifnot(inherits(test, "simpleError"))
 
-test <- tryCatch(t(as.symmetric(x)), error=identity)
+test <- tryCatch(xpose(as.symmetric(x)), error=identity)
 stopifnot(inherits(test, "simpleError"))
 
 
 
 ### real work
 band <- as.banded(x, 2, 1)
-test <- as.matrix(t(band))
-truth <- t(as.matrix(band))
+test <- as.matrix(xpose(band))
+truth <- xpose(as.matrix(band))
 stopifnot(all.equal(test, truth))
 
 band <- as.banded(x, 1, 2)
-test <- as.matrix(t(band))
-truth <- t(as.matrix(band))
+test <- as.matrix(xpose(band))
+truth <- xpose(as.matrix(band))
 stopifnot(all.equal(test, truth))
 
 band <- as.banded(x, 2, 2)
-test <- as.matrix(t(band))
-truth <- t(as.matrix(band))
+test <- as.matrix(xpose(band))
+truth <- xpose(as.matrix(band))
 stopifnot(all.equal(test, truth))
